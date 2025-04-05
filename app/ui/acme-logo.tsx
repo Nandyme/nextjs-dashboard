2923bfab90bfb74886e8e189d0d5240d022a8ccd
Nandyme <160822432+Nandyme@users.noreply.github.com>
2025-04-05 09:43:04 +0900

    First commit

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -1,13 +1,17 @@
-import { GlobeAltIcon } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
+import Image from 'next/image';
 
 export default function AcmeLogo() {
   return (
-    <div
-      className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
-    >
-      <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
-      <p className="text-[44px]">Acme</p>
+    <div className={`${lusitana.className} flex flex-row items-center leading-none text-white`}>
+      <Image
+        src="/i/seirai-logo.png"
+        alt="Seirai Logo"
+        width={120}
+        height={80}
+        priority
+      />
+      <p className="text-[12px] ml-2"></p>
     </div>
   );
 }
